5c4bd9d8d45fdc2f13e460ddf330262a85a581d2
MNGARCIA085 <mngarcia@mail.antel.com.uy>
2025-09-29 20:25:11 -0300

    dockerfile

---
+++ Dockerfile.ml
@@ -0,0 +1,34 @@
+# Use Debian base from Google's Docker Hub mirror
+# Dockerfile.ml
+FROM python:3.11-slim
+
+# System dependencies
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /app
+
+# Copy only what is needed for training
+COPY requirements.txt .
+RUN python3.11 -m pip install --no-cache-dir -r requirements.txt
+
+# Copy project code
+COPY config/ config
+COPY tests/ tests
+COPY src/ src/
+COPY scripts/ scripts/
+COPY data/ data/
+COPY notebooks/ notebooks/
+
+# Environment variables
+ENV PYTHONUNBUFFERED=1
+
+CMD ["bash"]
+
+
+
+
+#docker build -f Dockerfile.ml -t ml_env:latest .
+#docker run -it -v $(pwd):/app ml_env:latest
+#docker run -it -p 8080:8080 -v $(pwd):/app ml_env:latest; mlflow ui --host 0.0.0.0 --port 8080
